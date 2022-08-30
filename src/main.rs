df8369bd0d7999f912d75663b3c6fe08635879a8
Marcus Plienegger <marcus.plienegger@theguarantors.com>
2022-08-30 15:43:58 -0400

    add health_check endpoint and separate into lib and binary

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,7 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use rust_newsletter::run;
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
+// run: `cargo +nightly expand` (use nightly compiler for the 'expand' cmd only) to view macro expansion
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    run().await
 }
