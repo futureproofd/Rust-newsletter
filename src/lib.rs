261520661b38ee1c81f24bb797c5bee13c2d48ed
Marcus Plienegger <marcus.plienegger@theguarantors.com>
2022-09-22 12:28:55 -0400

    subscribe endpoint using deserialize extractor via serde

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,26 @@ use std::net::TcpListener;
 
 use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
 
+// We have to use the Deserialize macro from serde in order to extract FormData this way
+// Form::from_request tries to deserialise the body into FormData according to the rules of URL-encoding
+// leveraging serde_urlencoded and the Deserialize implementation of FormData, automatically generated for us by #[derive(serde::Deserialize)];
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
 async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+// All arguments in the signature of a route handler must implement the FromRequest trait: actix-web will invoke from_request for each argument.
+/// Extract FormData using serde (via extractor using from_request trait method)
+/// this handler gets called only if the content type is *x-www-form-urlencoded*
+/// All arguments in the signature of a route handler must implement the FromRequest trait: actix-web will invoke from_request
+/// for each argument and, if the extraction succeeds for all of
+/// them, it will then run the actual handler function.
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
@@ -12,9 +31,13 @@ async fn health_check() -> HttpResponse {
 // Notice the different signature!
 // We return `Server` on the happy path and we dropped the `async` keyword // We have no .await call, so it is not needed anymore.
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     //no more .await
     Ok(server)
 }
